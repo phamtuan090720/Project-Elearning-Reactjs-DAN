d075110ecc3c05e169cb2eb2b95449b2bb8bf8dd
Phạm Tuân <52484769+phamtuan090720@users.noreply.github.com>
2021-10-15 21:39:07 +0700

    fix bug token

--- src/api/setting.js
+++ src/api/setting.js
@@ -2,13 +2,13 @@ import axios from "axios";
 import cookies from 'react-cookies';
 export const DOMAIN = 'http://127.0.0.1:8000/';
 export const http = axios.create({
-    baseURL:DOMAIN,
-    timeout:3000,
+    baseURL: DOMAIN,
+    timeout: 3000,
 })
-http.interceptors.request.use((config)=>{
+http.interceptors.request.use((config) => {
     config.headers = {
         ...config.headers,
-        Authorization:`Bearer ${cookies.load('access_token')}`
+        Authorization: `${cookies.load('access_token') ? `${`Bearer ${cookies.load('access_token')}`}` : ''} `
     }
     return config
 }, (errors) => {
